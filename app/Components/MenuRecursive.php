222d6a81af610b5b9a315c7afe7ec9b616c263d4
Admin <NguyenDuyNam29032002>
2022-11-18 11:47:15 +0700

    push

--- app/Components/MenuRecursive.php
+++ app/Components/MenuRecursive.php
@@ -15,7 +15,7 @@ class MenuRecursive
     {
         $data = Menu::where('parent_id', $parent_id)->get();
         foreach ($data as $dataItem) {
-            $this->html .= '<option value="' . $dataItem->id . '">' . $subMark . $dataItem->name . '</option>';
+            $this->html .= '<option value="' . $dataItem->id . '">' . $subMark   . $dataItem->name . '</option>';
             $this->menuRecursiveAdd($dataItem->id, $subMark . '--');
         }
         return $this->html;
